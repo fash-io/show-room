68f7adb7782650b5a3f6625b3771eddcadec2b6d
Fash_ii <favourfasi46@gmail.com>
2024-09-17 13:53:44 +0100

    slider updt

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -110,47 +110,49 @@ const Slider = (props) => {
           transform: `translateX(-${currentIndex * 100}%)`,
         }}
       >
-        {movies.map((movie, index) => (
-          <div key={index} className="min-w-full relative h-full">
-            <img
-              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-              alt={movie.title || movie.name}
-              className={`w-full object-cover ${height}`}
-            />
-            <div className="absolute w-full px-4 sm:px-6 md:px-[6%] bottom-0 text-white bg-gradient-to-t from-black via-transparent to-transparent pb-4 sm:pb-6">
-              <h1
-                className="text-lg  sm:text-3xl md:text-4xl mb-4 md:mb-10 font-bold"
-                style={{ textShadow: "2px 2px 4px #000000" }}
-              >
-                {movie.title || movie.name}
-              </h1>
-
-              <p
-                className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-xl text-xs sm:text-xs lg:text-lg leading-5 mb-2 sm:mb-4 hidden md:block"
-                style={{ textShadow: "2px 2px 4px #000000" }}
-              >
-                {movie.overview}
-              </p>
-              <p
-                className="text-xs sm:text-sm md:text-md mb-2 uppercase tracking-wider text-slate-400 font-bold"
-                style={{ textShadow: "2px 2px 4px #000000" }}
-              >
-                {movie.media_type === "movie" ? "Movie" : "TV Show"}
-              </p>
-              <div className="flex gap-2 sm:gap-3 mb-4 sm:mb-8 md:mb-12">
-                <Link
-                  to={`/${movie.media_type === "movie" ? "movie" : "series"}/${
-                    movie.id
-                  }`}
-                  className="py-1 sm:py-2 flex px-3 sm:px-4 md:px-5 items-center gap-1 sm:gap-2 md:gap-3 text-xs sm:text-sm font-semibold bg-[#6d6d6eb3] rounded cursor-pointer hover:bg-[#6d6d6e66] transition duration-200"
+        {movies
+          .filter((movie) => movie.backdrop_path)
+          .map((movie, index) => (
+            <div key={index} className="min-w-full relative h-full">
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                alt={movie.title || movie.name}
+                className={`w-full object-cover ${height}`}
+              />
+              <div className="absolute w-full px-4 sm:px-6 md:px-[6%] bottom-0 text-white bg-gradient-to-t from-black via-transparent to-transparent pb-4 sm:pb-6">
+                <h1
+                  className="text-lg  sm:text-3xl md:text-4xl mb-4 md:mb-10 font-bold"
+                  style={{ textShadow: "2px 2px 4px #000000" }}
                 >
-                  <img src={info_icon} width={20} alt="Info Icon" />
-                  More Info
-                </Link>
+                  {movie.title || movie.name}
+                </h1>
+
+                <p
+                  className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-xl text-xs sm:text-xs lg:text-lg leading-5 mb-2 sm:mb-4 hidden md:block"
+                  style={{ textShadow: "2px 2px 4px #000000" }}
+                >
+                  {movie.overview}
+                </p>
+                <p
+                  className="text-xs sm:text-sm md:text-md mb-2 uppercase tracking-wider text-slate-400 font-bold"
+                  style={{ textShadow: "2px 2px 4px #000000" }}
+                >
+                  {movie.media_type === "movie" ? "Movie" : "TV Show"}
+                </p>
+                <div className="flex gap-2 sm:gap-3 mb-4 sm:mb-8 md:mb-12">
+                  <Link
+                    to={`/${
+                      movie.media_type === "movie" ? "movie" : "series"
+                    }/${movie.id}`}
+                    className="py-1 sm:py-2 flex px-3 sm:px-4 md:px-5 items-center gap-1 sm:gap-2 md:gap-3 text-xs sm:text-sm font-semibold bg-[#6d6d6eb3] rounded cursor-pointer hover:bg-[#6d6d6e66] transition duration-200"
+                  >
+                    <img src={info_icon} width={20} alt="Info Icon" />
+                    More Info
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       {/* Right button for manual navigation */}
