7aafa3d96fa483be9427a2388f831dc4f4c17ec5
Fash_ii <favourfasi46@gmail.com>
2025-01-03 03:27:47 +0100

    kk

--- src/pages/PopularPage.jsx
+++ src/pages/PopularPage.jsx
@@ -146,7 +146,7 @@ const PopularPage = () => {
           ))}
         </div>
       </div>
-      <div className='sm:px-20 sm:py-14 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-5 sm:gap-y-8'>
+      <div className='sm:px-20 sm:py-14 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-5 sm:gap-y-8'>
         {popularMovies
           .filter(movie => movie.poster_path)
           .map(movie => (
